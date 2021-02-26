41a6c63a7e6c2b59aa8093f15962ce3bdd4d9d35
William Adam-Grenier <william.a-g@hotmail.com>
2021-02-25 20:37:50 -0500

    Update g3dBoundingVolume.h

--- src/Graphics/g3d/g3dBoundingVolume.h
+++ src/Graphics/g3d/g3dBoundingVolume.h
@@ -9,8 +9,6 @@ bool g3dbvIsEllipseInclude(float const (*)[3] param1, float const *param2);
 
 void g3dbvInnerEllipseFromVertices(float (*)[3] param1, float const (*)[3] param2);
 
-void g3dbvBoxFromVertices(float (*)[3] param1, float const (*)[3])
-
-param2;
+void g3dbvBoxFromVertices(float (*)[3] param1, float const (*)[3] param2);
 
 #endif //FATALSTACK_G3DBOUNDINGVOLUME_H
